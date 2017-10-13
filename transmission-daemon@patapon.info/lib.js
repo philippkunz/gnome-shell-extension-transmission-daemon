12a678cfa3bd88a55564f8e6459c17ce86e674c8
Kevin James <KevinJames@thekev.in>
2017-10-12 21:11:11 -0700

    style(lib): code cleanup

--- transmission-daemon@patapon.info/lib.js
+++ transmission-daemon@patapon.info/lib.js
@@ -21,20 +21,19 @@ function getSettings(extension) {
     let schemaName = 'org.gnome.shell.extensions.transmission-daemon';
     let schemaDir = extension.dir.get_child('schemas').get_path();
 
-    // Extension installed in .local
     if (GLib.file_test(schemaDir + '/gschemas.compiled', GLib.FileTest.EXISTS)) {
-        let schemaSource = Gio.SettingsSchemaSource.new_from_directory(schemaDir,
-                                  Gio.SettingsSchemaSource.get_default(),
-                                  false);
+        // extension installed in .local
+        let schemaSource = Gio.SettingsSchemaSource.new_from_directory(
+            schemaDir, Gio.SettingsSchemaSource.get_default(), false);
         let schema = schemaSource.lookup(schemaName, false);
 
         return new Gio.Settings({ settings_schema: schema, });
-    }
-    // Extension installed system-wide
-    else {
-        if (Gio.Settings.list_schemas().indexOf(schemaName) == -1) {
+    } else {
+        // extension installed system-wide
+        if (Gio.Settings.list_schemas().indexOf(schemaName) === -1) {
             throw "Schema \"%s\" not found.".format(schemaName);
         }
+
         return new Gio.Settings({ schema: schemaName, });
     }
 }
@@ -42,12 +41,11 @@ function getSettings(extension) {
 function initTranslations(extension) {
     let localeDir = extension.dir.get_child('locale').get_path();
 
-    // Extension installed in .local
     if (GLib.file_test(localeDir, GLib.FileTest.EXISTS)) {
+        // extension installed in .local
         Gettext.bindtextdomain('gnome-shell-extension-transmission-daemon', localeDir);
-    }
-    // Extension installed system-wide
-    else {
+    } else {
+        // extension installed system-wide
         Gettext.bindtextdomain('gnome-shell-extension-transmission-daemon', extension.metadata.locale);
     }
 }
